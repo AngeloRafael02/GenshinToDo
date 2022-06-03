5365257a5da1d504903ae022c021d97faae4cb90
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-06-03 21:46:21 +0800

    changed db name

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -1,9 +1,9 @@
 const {Pool} = require('pg')
 
 const pool = new Pool({
-    user:'postgres',
+    user:'postgres', //currently using Main User (Make a dedicated user + password for app)
     password:'820824',
-    database:'genshin_impact',
+    database:'test4_db',
     port: 5432,
     host:'localhost',
 });
