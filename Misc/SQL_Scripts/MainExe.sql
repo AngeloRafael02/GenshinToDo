6f42dcd4b802d681dc48376a8734adb55218fc4a
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-06-17 22:18:40 +0800

    Add methods for querying Weapons

--- Misc/SQL_Scripts/MainExe.sql
+++ Misc/SQL_Scripts/MainExe.sql
@@ -1,12 +1,10 @@
 --run through the psql shell
 --to tun this main Exe Command: \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/MainExe.sql
-CREATE DATABASE typeormdb;
+CREATE DATABASE freedb2;
 \set ON_ERROR_STOP 1
-begin;
-    \c typeormdb
+    \c freedb2
     \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/CreateAllTables.sql
     \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/InsertSideTableData.sql
     \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/CreateCharactersData.sql
     \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/CreateWeaponsData.sql
     \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/CreateViews.sql
-commit;
