b7c0eb3e9c96bf64e42a06f336fff01724f0ac15
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-06-17 22:17:32 +0800

    changed user, still superuser Tho

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -1,8 +1,8 @@
 const {Pool} = require('pg')
 
 const pool = new Pool({
-    user:'postgres', //currently using Main User (Make a dedicated user + password for app)
-    password:'820824',
+    user:'user123', //currently using Main User (Make a dedicated user + password for app)
+    password:'password123',
     database:'test4_db',
     port: 5432,
     host:'localhost',
