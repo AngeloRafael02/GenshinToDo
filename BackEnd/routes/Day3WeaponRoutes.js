f39a1571f5074c7eafcf17d84810bd40b7b2e158
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-06-18 20:57:37 +0800

    FixedBug: fixed Query showing different response

--- BackEnd/routes/Day3WeaponRoutes.js
+++ BackEnd/routes/Day3WeaponRoutes.js
@@ -15,7 +15,7 @@ Day3WeaponRoutes.get(['/All'],async function (req,res) {
 Day3WeaponRoutes.get(['/Mondstadt'],async function (req,res) {
     try{
         const WeaponResponse = await pool.query(
-            "SELECT * FROM Day2Weapons WHERE material = 'Dandelion Gladiator';");
+            "SELECT * FROM Day3Weapons WHERE material = 'Dandelion Gladiator';");
         res.json(WeaponResponse.rows);
     } catch(error){
         console.log(error)
@@ -24,7 +24,7 @@ Day3WeaponRoutes.get(['/Mondstadt'],async function (req,res) {
 Day3WeaponRoutes.get(['/Liyue'],async function (req,res) {
     try{
         const WeaponResponse = await pool.query(
-            "SELECT * FROM Day2Weapons WHERE material = 'Aerosiderite';");
+            "SELECT * FROM Day3Weapons WHERE material = 'Aerosiderite';");
         res.json(WeaponResponse.rows);
     } catch(error){
         console.log(error)
@@ -33,7 +33,7 @@ Day3WeaponRoutes.get(['/Liyue'],async function (req,res) {
 Day3WeaponRoutes.get(['/Inazuma'],async function (req,res) {
     try{
         const WeaponResponse = await pool.query(
-            "SELECT * FROM Day2Weapons WHERE material = 'Mask';");
+            "SELECT * FROM Day3Weapons WHERE material = 'Mask';");
         res.json(WeaponResponse.rows);
     } catch(error){
         console.log(error)
