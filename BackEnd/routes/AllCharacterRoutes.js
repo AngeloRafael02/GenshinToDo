6c9492b5ef14cb49e2351201094601321748be78
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-06-25 21:10:23 +0800

    Changed SQL Command According to FrontEnd format

--- BackEnd/routes/AllCharacterRoutes.js
+++ BackEnd/routes/AllCharacterRoutes.js
@@ -6,7 +6,16 @@ const express = require('express'),
 CharRouter.get('/All', async function(req,res){
     try{
         const charResponse = await pool.query(
-            "SELECT * FROM AllCharacters;");
+           `SELECT characters.id, name, star, Elements.Element, WeaponTypes.Type, Regions.Region, AscensionDomains.DomainName, CharacterDomainMaterials.Material, Availability.Days, ImgURL
+            FROM Characters
+            JOIN Elements ON Characters.Element_id = Elements.id
+            JOIN WeaponTypes ON Characters.WeaponType_id = WeaponTypes.id
+            JOIN Regions ON Characters.Region_id = Regions.id
+            JOIN AscensionDomains ON Characters.Domain_id = AscensionDomains .id
+            JOIN CharacterDomainMaterials ON Characters.DomainMaterial_id = CharacterDomainMaterials.id
+            JOIN Availability ON Characters.Availabilties = Availability.id
+            WHERE Availabilties IN (1,2,3)
+            ORDER BY AscensionDomains.id, characters.id ASC;`);
         res.json(charResponse.rows);
     } catch (error){
         console.log(error); 
