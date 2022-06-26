f1b2f777ab60e8bf031a218b853fd08efca0dd36
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-06-26 21:54:17 +0800

    fixed '/all' routes according to FrontEnd request

--- BackEnd/routes/Day3WeaponRoutes.js
+++ BackEnd/routes/Day3WeaponRoutes.js
@@ -6,7 +6,15 @@ Day3WeaponRoutes = express.Router(),
 Day3WeaponRoutes.get(['/All'],async function (req,res) {
     try{
         const WeaponResponse = await pool.query(
-            "SELECT * FROM Day3Weapons;");
+           `SELECT Weapons.id, name, star,  WeaponTypes.Type, SecondStats.Stat, AscensionDomains.DomainName, WeaponDomainMaterials.Material, Availability.Days, ImgURL
+            FROM Weapons
+            JOIN SecondStats ON Weapons.SecondStat_ID = SecondStats.id
+            JOIN WeaponTypes ON Weapons.WeaponType_ID = WeaponTypes.id
+            JOIN AscensionDomains ON Weapons.Domain_ID = AscensionDomains .id
+            JOIN WeaponDomainMaterials ON Weapons.DomainMaterial_id = WeaponDomainMaterials.id
+            JOIN Availability ON Weapons.Availabilties = Availability.id
+            WHERE Availabilties = 3
+            ORDER BY WeaponDomainMaterials.id, star, WeaponTypes.id, Weapons.id ASC;`);
         res.json(WeaponResponse.rows);
     } catch(error){
         console.log(error)
