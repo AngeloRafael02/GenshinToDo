397d14197104c28bd951da79c5de33299a5db048
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-06-27 19:31:47 +0800

    changed  database

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -3,7 +3,7 @@ const {Pool} = require('pg')
 const pool = new Pool({
     user:'user123', //currently using Main User (Make a dedicated user + password for app)
     password:'password123',
-    database:'test4_db',
+    database:'db1',
     port: 5432,
     host:'localhost',
 });
