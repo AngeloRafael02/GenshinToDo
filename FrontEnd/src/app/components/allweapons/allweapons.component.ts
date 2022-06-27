d256f5c7e1a5cc306085460226b5e166b4345d0e
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-06-27 19:43:38 +0800

    added inheritance from the Base Component

--- FrontEnd/src/app/components/allweapons/allweapons.component.ts
+++ FrontEnd/src/app/components/allweapons/allweapons.component.ts
@@ -1,37 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { weaponInterface } from '../../interfaces';
-import { WeaponService } from '../../main.service';
+import { Component, Injector, OnInit } from '@angular/core';
+import { BaseWeaponClassComponent } from '../Utils/base-weapon-class.component';
 
 @Component({
   selector: 'app-allweapons',
   template: `
   <p>All Materials (Cecilia Garden, Mondstadt)</p>
   <div class="container">
-  <ul  *ngFor="let Weapon of MondstadtWeapons" >
+  <ul  *ngFor="let Weapon of MondstadtWeapons; let i = index;" >
       <li >
           <abbr title="{{ Weapon.name }}">
-          <img class="WeaponPic" src="{{ Weapon.imgurl }}" alt="{{ Weapon.name }}" (click)="onSelect(Weapon)" >
+          <img class="WeaponPic" src="{{ Weapon.imgurl }}" alt="{{ Weapon.name }}" (click)="onSelect2(i+1)" >
           </abbr>
       </li>
   </ul>
   </div>
   <p>All Materials (Hidden Palace of Lianshan Formula, Liyue)</p>
   <div class="container">
-  <ul  *ngFor="let Weapon of LiyueWeapons" >
+  <ul  *ngFor="let Weapon of LiyueWeapons; let i = index;" >
       <li >
           <abbr title="{{ Weapon.name }}">
-          <img class="WeaponPic" src="{{ Weapon.imgurl }}" alt="{{ Weapon.name }}" (click)="onSelect(Weapon)" >
+          <img class="WeaponPic" src="{{ Weapon.imgurl }}" alt="{{ Weapon.name }}" (click)="onSelect2(i+56)" >
           </abbr>
       </li>
   </ul>
   </div>
   <p>All Materials (Hidden Palace of Lianshan Formula, Liyue)</p>
   <div class="container">
-  <ul  *ngFor="let Weapon of InazumaWeapons" >
+  <ul  *ngFor="let Weapon of InazumaWeapons; let i = index;" >
       <li >
           <abbr title="{{ Weapon.name }}">
-          <img class="WeaponPic" src="{{ Weapon.imgurl }}" alt="{{ Weapon.name }}" (click)="onSelect(Weapon)" >
+          <img class="WeaponPic" src="{{ Weapon.imgurl }}" alt="{{ Weapon.name }}" (click)="onSelect2(i+106)" >
           </abbr>
       </li>
   </ul>
@@ -40,24 +38,24 @@ import { WeaponService } from '../../main.service';
   `,
   styleUrls: ['allweapons.component.scss']
 })
-export class AllWeaponsComponent implements OnInit {
+export class AllWeaponsComponent extends BaseWeaponClassComponent implements OnInit {
 
-  public selectedID = 0;
-  public MondstadtWeapons:weaponInterface[] = [];
-  public LiyueWeapons:weaponInterface[]=[];
-  public InazumaWeapons:weaponInterface[]=[];
+  public selectedID = super.Id;
+  public MondstadtWeapons = super.Weapons;
+  public LiyueWeapons = super.Weapons;
+  public InazumaWeapons = super.Weapons;
 
-  constructor(
-    private _weaponService:WeaponService,
-    private router:Router
-  ) { }
+  constructor(WeaponClassInjector:Injector) {
+     super(WeaponClassInjector)
+  }
 
   ngOnInit(): void {
     this._weaponService.getAllDaysWeapons(1).subscribe(data => this.MondstadtWeapons = data)
     this._weaponService.getAllDaysWeapons(2).subscribe(data => this.LiyueWeapons = data)
     this._weaponService.getAllDaysWeapons(3).subscribe(data => this.InazumaWeapons = data)
   }
-  onSelect(Weapon:weaponInterface):void{ //navigates to a route according to the id number of the character
-    this.router.navigate(['/Weapons/allweapons', Weapon.id]);
+
+  onSelect2(index:number){  
+    this.router.navigate(['/Weapons/allweapons',index]);
   }
 }
--- FrontEnd/src/app/components/day1weapons/day1weapons.component.ts
+++ FrontEnd/src/app/components/day1weapons/day1weapons.component.ts
@@ -44,20 +44,16 @@ export class Day1weaponsComponent extends BaseWeaponClassComponent implements On
   public LiyueWeapons = super.Weapons;
   public InazumaWeapons= super.Weapons;
 
-  constructor(
-   WeaponClassInjector:Injector
-  ) {
+  constructor(WeaponClassInjector:Injector) {
     super(WeaponClassInjector)
-   }
+  }
 
   ngOnInit(): void {
     this._weaponService.getDay1Weapons(1).subscribe(data => this.MondstadtWeapons = data)
     this._weaponService.getDay1Weapons(2).subscribe(data => this.LiyueWeapons = data);
     this._weaponService.getDay1Weapons(3).subscribe(data => this.InazumaWeapons = data);
   }
-  onSelect(Weapon:weaponInterface):void{
-    this.router.navigate(['/Weapons/day1weapons',Weapon.id])
-  }
+
   onSelect2(index:number){  
     this.router.navigate(['/Weapons/day1weapons',index]);
   }
